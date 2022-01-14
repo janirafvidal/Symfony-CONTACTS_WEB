1fb16b2cea0ad70bba4cdd2fda78aaaafb3fb781
JaniraFV <alufav700@ieselcaminas.org>
2022-01-14 11:30:55 +0100

    2.6.1 finished

--- src/Controller/ContactoController.php
+++ src/Controller/ContactoController.php
@@ -2,13 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Contacto;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
 
 class ContactoController extends AbstractController
 {
-
+    
     private $contactos = [
         1 => ["nombre" => "Juan Pérez", "telefono" => "524142432", "email" => "juanp@ieselcaminas.org"],
         2 => ["nombre" => "Ana López", "telefono" => "58958448", "email" => "anita@ieselcaminas.org"],
@@ -16,6 +18,29 @@ class ContactoController extends AbstractController
         7 => ["nombre" => "Laura Martínez", "telefono" => "42898966", "email" => "lm2000@ieselcaminas.org"],
         9 => ["nombre" => "Nora Jover", "telefono" => "54565859", "email" => "norajover@ieselcaminas.org"]
     ]; 
+    
+    /**
+     * @Route("/contacto/insertar", name="insertar_contacto")
+     */
+    public function insertar(ManagerRegistry $doctrine)
+    {
+        $entityManager = $doctrine->getManager();
+        foreach($this->contactos as $c){
+            $contacto = new Contacto();
+            $contacto->setNombre($c["nombre"]);
+            $contacto->setTelefono($c["telefono"]);
+            $contacto->setEmail($c["email"]);
+            $entityManager->persist($contacto);
+        }
+
+        try{
+            $entityManager->flush();
+            return new Response("Contactos insertados");
+        }catch(\Exception $e){
+            return new Response("Error insertando objetos");
+        }
+    }
+
 
     /**
     * @Route("/contacto/{codigo}", name="ficha_contacto")
@@ -46,4 +71,6 @@ class ContactoController extends AbstractController
             ]);
 
     }
+
+
 }
